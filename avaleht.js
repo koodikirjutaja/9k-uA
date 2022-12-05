55e8b49a738ee614ebef55fa8d4833dcbc2715a9
Jakobson03 <jakobson.michelle@gmail.com>
2022-12-05 20:26:32 +0200

    modified:   avaleht.js tegin javascripti ilusamaks

--- avaleht.js
+++ avaleht.js
@@ -1,36 +1,37 @@
 /* sandori kirjutatud on myFunction, Mihkli kirjutatud on nuppude värvivahetused */
 
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
+  var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+            x.className += " responsive";
+        } else {
+            x.className = "topnav";
+                }
+}
 
 /*teeb esimese nupu värvi lillaks */
 function värvimuutus1() { 
-  var v = document.getElementById("hiir1"); {
-    v.style.backgroundColor = "#27153E";
-};
+var v = document.getElementById("hiir1"); {
+  v.style.backgroundColor = "#27153E";
+}
 }
 /*teeb esimese nupu värvi siniseks */
 function värvimuutus1tagasi() {
-  var v = document.getElementById("hiir1"); {
-    v.style.backgroundColor = "#5140FF";
-};
+var v = document.getElementById("hiir1"); {
+  v.style.backgroundColor = "#5140FF";
+}
 }
 /*teeb teise nupu värvi lillaks */
 function värvimuutus2() {
-  var v = document.getElementById("hiir2"); {
-    v.style.backgroundColor = "#27153E";
-};
+var v = document.getElementById("hiir2"); {
+  v.style.backgroundColor = "#27153E";
+}
 }
 
 /*teeb teise nupu värvi lillaks */
 function värvimuutus2tagasi() {
-  var v = document.getElementById("hiir2"); {
-    v.style.backgroundColor = "#5140FF";
-};
+var v = document.getElementById("hiir2"); {
+  v.style.backgroundColor = "#5140FF";
 }
+}
+
