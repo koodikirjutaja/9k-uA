a3dd4eb9ec8b73d5ab5704d3b6125abd90f33884
Jakobson03 <jakobson.michelle@gmail.com>
2022-12-05 20:01:28 +0200

    modified:   avaleht.html modified:   avaleht.js lisasin nuppudevärvimuutuse ja kommeteerisin natuke

--- avaleht.js
+++ avaleht.js
@@ -9,8 +9,28 @@ function myFunction() {
     }
   }
 
-function värvimuutus() {
-  var v = document.getElementById("hiir").onclick = function () {
+/*teeb esimese nupu värvi lillaks */
+function värvimuutus1() { 
+  var v = document.getElementById("hiir1"); {
     v.style.backgroundColor = "#27153E";
+};
+}
+/*teeb esimese nupu värvi siniseks */
+function värvimuutus1tagasi() {
+  var v = document.getElementById("hiir1"); {
+    v.style.backgroundColor = "#5140FF";
+};
+}
+/*teeb teise nupu värvi lillaks */
+function värvimuutus2() {
+  var v = document.getElementById("hiir2"); {
+    v.style.backgroundColor = "#27153E";
+};
+}
+
+/*teeb teise nupu värvi lillaks */
+function värvimuutus2tagasi() {
+  var v = document.getElementById("hiir2"); {
+    v.style.backgroundColor = "#5140FF";
 };
 }
