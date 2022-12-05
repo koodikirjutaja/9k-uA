e3678089456b9f2f1d0b5787b9d935cef1e322a2
Rlepik11 <116276854+Rlepik11@users.noreply.github.com>
2022-12-05 13:12:23 +0200

    Add files via upload

--- kontakt1.js
+++ kontakt1.js
@@ -1,9 +1,16 @@
+
 let text = document.getElementById("text1").innerHTML;
-  const copyContent = async () => {
-    try {
-      await navigator.clipboard.writeText(text);
-      console.log('Content copied to clipboard');
-    } catch (err) {
-      console.error('Failed to copy: ', err);
+  
+const copyContent = async () => {
+    
+  try {
+      
+    await navigator.clipboard.writeText(text);
+      
+    console.log('Content copied to clipboard');
+    
+  } catch (err) {
+      
+    console.error('Failed to copy: ', err);
     }
   }
